d58c77762bfb0e17cac113cb3231eac4eb2a2e10
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-14 13:51:10 +0100

    add explanatory comments

--- js/ui/app/helpers/api.js
+++ js/ui/app/helpers/api.js
@@ -26,6 +26,7 @@ export default (method, url, body={}, headers={}) =>
         }, headers)
       };
 
+      // dissalow body inclusion for methods that don't support it
       if(method !== 'GET' && method !== 'DELETE') {
         options['body'] = JSON.stringify(body);
       }
@@ -33,6 +34,7 @@ export default (method, url, body={}, headers={}) =>
       const response = await fetch(url, options);
       if(response.status === 401) window.location.href = '/login';
       if(response.status >= 400) throw new Error(response.status);
+      // including the deleted resource is usefull for any further actions
       if(method === 'DELETE' && response.status === 204) return resolver.resolve({result: body});
 
       const json = await response.json()
