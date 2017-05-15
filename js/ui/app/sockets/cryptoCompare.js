e25e860cb4c218ae611d9f9ce98df4a32d9bd8c6
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-15 20:30:33 +0100

    add most stream lib and create btc and eth connections

---
+++ js/ui/app/sockets/cryptoCompare.js
@@ -0,0 +1,77 @@
+import io from 'socket.io-client';
+import {create} from '@most/create'
+
+const CURRENTAGG = '2';
+const URL = 'https://streamer.cryptocompare.com/';
+
+const FIELDS = {
+    'TYPE'            : 0x0
+  , 'MARKET'          : 0x0
+  , 'FROMSYMBOL'      : 0x0
+  , 'TOSYMBOL'        : 0x0
+  , 'FLAGS'           : 0x0
+  , 'PRICE'           : 0x1
+  , 'BID'             : 0x2
+  , 'OFFER'           : 0x4
+  , 'LASTUPDATE'      : 0x8
+  , 'AVG'             : 0x10
+  , 'LASTVOLUME'      : 0x20
+  , 'LASTVOLUMETO'    : 0x40
+  , 'LASTTRADEID'     : 0x80
+  , 'VOLUMEHOUR'      : 0x100
+  , 'VOLUMEHOURTO'    : 0x200
+  , 'VOLUME24HOUR'    : 0x400
+  , 'VOLUME24HOURTO'  : 0x800
+  , 'OPENHOUR'        : 0x1000
+  , 'HIGHHOUR'        : 0x2000
+  , 'LOWHOUR'         : 0x4000
+  , 'OPEN24HOUR'      : 0x8000
+  , 'HIGH24HOUR'      : 0x10000
+  , 'LOW24HOUR'       : 0x20000
+  , 'LASTMARKET'      : 0x40000
+};
+
+const unpack = value => {
+  const valuesArray = value.split("~");
+  const valuesArrayLenght = valuesArray.length;
+  const mask = valuesArray[valuesArrayLenght-1];
+  const maskInt = parseInt(mask,16);
+  const unpackedCurrent = {};
+  let currentField = 0;
+
+  for(const property in FIELDS) {
+      if(FIELDS[property] === 0) {
+        unpackedCurrent[property] = valuesArray[currentField];
+        currentField++;
+      }
+      else if(maskInt&FIELDS[property]) {
+        if(property === 'LASTMARKET'){
+          unpackedCurrent[property] = valuesArray[currentField];
+        }
+        else{
+          unpackedCurrent[property] = parseFloat(valuesArray[currentField]);
+        }
+        currentField++;
+      }
+  }
+
+  return unpackedCurrent;
+};
+
+export const connect = (symbol, exchangeName) => {
+  const subscription = [`2~${exchangeName}~${symbol}~GBP`];
+  const socket = io.connect(URL);
+  socket.emit('SubAdd', {subs:subscription});
+
+  return create((add, end, error) => {
+    socket.on('m', message => {
+      const messageType = message.substring(0, message.indexOf("~"));
+
+      if (messageType === CURRENTAGG) {
+        add(unpack(message));
+      }
+    });
+
+    return () => socket.emit('SubRemove', {subs:subscription});
+  });
+}
--- js/ui/app/web/dashboard/Overview.jsx
+++ js/ui/app/web/dashboard/Overview.jsx
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
+import {merge} from 'most';
+import {connect} from '../../sockets/cryptoCompare';
 
 export default class Overview extends Component {
+  componentDidMount() {
+    const btc$ = connect('BTC', 'Coinfloor')
+    const eth$ = connect('ETH', 'Kraken');
+
+    merge(btc$, eth$)
+      .map(v => ({
+        price: v.PRICE,
+        market: v.MARKET,
+        symbol: v.FROMSYMBOL
+      }))
+      .forEach(values => {
+        console.log(values);
+      })
+  }
   render() {
     return (
       <p>Dashboard</p>
