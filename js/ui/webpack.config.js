f81818b8d115c817d345a74f1dbd3494dc3cbe05
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-22 21:18:00 +0100

    disable uglify for the time being

--- js/ui/webpack.config.js
+++ js/ui/webpack.config.js
@@ -29,23 +29,23 @@ module.exports = () => {
         minimize: true,
         debug: false
       }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-          screw_ie8: true,
-          conditionals: true,
-          unused: true,
-          comparisons: true,
-          sequences: true,
-          dead_code: true,
-          evaluate: true,
-          if_return: true,
-          join_vars: true,
-        },
-        output: {
-          comments: false,
-        },
-      })
+      // new webpack.optimize.UglifyJsPlugin({
+      //   compress: {
+      //     warnings: false,
+      //     screw_ie8: true,
+      //     conditionals: true,
+      //     unused: true,
+      //     comparisons: true,
+      //     sequences: true,
+      //     dead_code: true,
+      //     evaluate: true,
+      //     if_return: true,
+      //     join_vars: true,
+      //   },
+      //   output: {
+      //     comments: false,
+      //   },
+      // })
     );
   }
   else {
