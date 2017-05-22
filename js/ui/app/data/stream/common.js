95170f4e76d955f60a05603b977c6174b0c89904
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-22 19:16:51 +0100

    fix the prefetch of partial investments

--- js/ui/app/data/stream/common.js
+++ js/ui/app/data/stream/common.js
@@ -6,13 +6,13 @@ const INVESTMENT_ENDPOINT = `${process.env.API_URL}/investments`;
 const historicalDataUrl = (fromSymbol, toSymbol, timestamp, days) =>
   `https://min-api.cryptocompare.com/data/histoday?fsym=${fromSymbol}&tsym=${toSymbol}&limit=${days}&aggregate=1&toTs=${timestamp}`
 
-const fetchPartialInvestments = fetch('GET', `${INVESTMENT_ENDPOINT}/partial`).run();
+const fetchPartialInvestments = fetch('GET', `${INVESTMENT_ENDPOINT}/partial`);
 const fetchBTC = fetch('GET', historicalDataUrl('BTC', 'GBP', +(new Date), 30), {}, false);
 const fetchETH = fetch('GET', historicalDataUrl('ETH', 'GBP', +(new Date), 30), {}, false);
 
 export const getBTC$ = () => fromPromise(fetchBTC.run().promise());
 export const getETH$ = () => fromPromise(fetchETH.run().promise());
-export const getPartialInvestment$ = () => fromPromise(fetchPartialInvestments.promise());
+export const getPartialInvestment$ = () => fromPromise(fetchPartialInvestments.run().promise());
 
 export const getPriceObjFromStreamData = data => ({
   price: data.PRICE,
