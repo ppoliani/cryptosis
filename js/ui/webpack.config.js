942ad285675996255b4f1b0b41dfdea6ad0abb3d
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-22 23:11:02 +0100

    remove prod webpack configs

--- js/ui/webpack.config.js
+++ js/ui/webpack.config.js
@@ -21,32 +21,32 @@ module.exports = () => {
   ];
 
   if (isProd) {
-    plugins.push(
-      new DotenvPlugin({
-        path: './.env.production'
-      }),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      })
-      // new webpack.optimize.UglifyJsPlugin({
-      //   compress: {
-      //     warnings: false,
-      //     screw_ie8: true,
-      //     conditionals: true,
-      //     unused: true,
-      //     comparisons: true,
-      //     sequences: true,
-      //     dead_code: true,
-      //     evaluate: true,
-      //     if_return: true,
-      //     join_vars: true,
-      //   },
-      //   output: {
-      //     comments: false,
-      //   },
-      // })
-    );
+    // plugins.push(
+    //   new DotenvPlugin({
+    //     path: './.env.production'
+    //   }),
+    //   new webpack.LoaderOptionsPlugin({
+    //     minimize: true,
+    //     debug: false
+    //   }),
+    //   new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //       warnings: false,
+    //       screw_ie8: true,
+    //       conditionals: true,
+    //       unused: true,
+    //       comparisons: true,
+    //       sequences: true,
+    //       dead_code: true,
+    //       evaluate: true,
+    //       if_return: true,
+    //       join_vars: true,
+    //     },
+    //     output: {
+    //       comments: false,
+    //     },
+    //   })
+    // );
   }
   else {
     plugins.push(
