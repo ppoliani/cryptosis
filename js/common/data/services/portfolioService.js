4d11fc693836ac9d349957bb6582835b8bb702de
Pavlos Polianidis <ppoliani@gmail.com>
2017-05-28 18:31:29 +0100

    add more tests for calculatePortfolioState

--- js/common/data/services/portfolioService.js
+++ js/common/data/services/portfolioService.js
@@ -1,25 +1,26 @@
 const {Map} = require('immutable');
 const {getInvestmentValueChange} = require('../../../common/aggregators/common');
 
-const calculatePortfolioState = (investments, qtySold, priceSold) => {
-	return investments.reduce((acc, investment, investmentId) => {
-    if(acc.get('quantity') > 0) {
-      const diff = investment.get('quantity') - acc.get('quantity');
-      const qtyUsed = diff < 0 ? investment.get('quantity') : qtySold;
+const calculatePortfolioState = (investments, qtySold, priceSold) => investments
+  .reduce((acc, investment, investmentId) => {
+      const qty = acc.get('quantity');
 
-      return acc
-        .set('quantity', Math.abs(Math.min(diff, 0)))
-        .setIn(['investments', investmentId], Map({
-          qtyUsed,
-          qtyLeft: Math.max(diff, 0),
-          gain: getInvestmentValueChange(qtyUsed, investment.get('price'), priceSold)
-        }))
-    }
+      if(qty > 0) {
+        const diff = investment.get('quantity') - qty;
+        const qtyUsed = diff < 0 ? investment.get('quantity') : qty;
 
-    return acc;
-  }, Map({investments: Map(), quantity: qtySold}))
+        return acc
+          .set('quantity', Math.abs(Math.min(diff, 0)))
+          .setIn(['investments', investmentId], Map({
+            qtyUsed,
+            qtyLeft: Math.max(diff, 0),
+            gain: getInvestmentValueChange(qtyUsed, investment.get('price'), priceSold)
+          }))
+      }
+
+      return acc;
+    }, Map({investments: Map(), quantity: qtySold}))
   .get('investments')
-}
 
 
 const calculatePortfolioValueOnSellAdded = (investments, sell) => {
--- js/common/data/services/portfolioService.test.js
+++ js/common/data/services/portfolioService.test.js
@@ -31,3 +31,81 @@ test('calculatePortfolioState should reduce the amount of the first investment i
     }
   )
 })
+
+
+test('calculatePortfolioState should fully consume the quantity of the first investment', t => {
+  t.deepEqual(
+    calculatePortfolioState(t.context.investments, 5, 1500).toJS(),
+    {
+      '1': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1000, 1500)
+      }
+    }
+  )
+})
+
+test('calculatePortfolioState should reduce the amount of the first and the others if quantity sold is big enough', t => {
+  t.deepEqual(
+    calculatePortfolioState(t.context.investments, 7, 1700).toJS(),
+    {
+      '1': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1000, 1700)
+      },
+      '2': {
+        qtyUsed: 2,
+        qtyLeft: 3,
+        gain: getGain(2, 1500, 1700)
+      }
+    }
+  )
+})
+
+test('calculatePortfolioState should reduce the amount of the first and all others if quantity sold is big enough', t => {
+  t.deepEqual(
+    calculatePortfolioState(t.context.investments, 12, 1700).toJS(),
+    {
+      '1': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1000, 1700)
+      },
+      '2': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1500, 1700)
+      },
+      '3': {
+        qtyUsed: 2,
+        qtyLeft: 4,
+        gain: getGain(2, 2000, 1700)
+      }
+    }
+  )
+})
+
+test('calculatePortfolioState should fully consume all investment if the qty sold is equal to the sum of all investment quantities ', t => {
+  t.deepEqual(
+    calculatePortfolioState(t.context.investments, 16, 1700).toJS(),
+    {
+      '1': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1000, 1700)
+      },
+      '2': {
+        qtyUsed: 5,
+        qtyLeft: 0,
+        gain: getGain(5, 1500, 1700)
+      },
+      '3': {
+        qtyUsed: 6,
+        qtyLeft: 0,
+        gain: getGain(6, 2000, 1700)
+      }
+    }
+  )
+})
