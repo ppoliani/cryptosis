e1c0bea4bf2b6e44ff8adc11bfd652ec38293719
Pavlos Polianidis <ppoliani@gmail.com>
2017-08-15 18:13:13 +0100

    update the widgets when fiat currency changes

--- js/ui/app/web/dashboard/Widget.jsx
+++ js/ui/app/web/dashboard/Widget.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react'
+import pureComponent from '../mixins/pureComponent'
 
 // That's how crypto compare code knows about the theme :(
 window.cccTheme =
     {"General":{"background":"#333","borderColor":"#121212"},"PoweredBy":{"textColor":"#EEE","linkColor":"#ffcc66"},"Data":{"priceColor":"#FFF","infoValueColor":"#FFF","borderColor":"#333"},"NewsItem":{"color":"#FFF","borderColor":"#444"},"Conversion":{"background":"#000","color":"#CCC"}};
 
+@pureComponent
 class Widget extends Component {
   componentDidMount() {
+    this.addScript();
+  }
+
+  componentDidUpdate() {
+    this.container.querySelector('script').remove();
+    this.container.querySelector('.ccc-widget').remove();
+    this.addScript();
+  }
+
+  addScript() {
     const s = document.createElement('script');
     s.src = this.props.src;
     s.async = true;
--- js/ui/app/web/dashboard/tabs/ExtraInfoTabView.jsx
+++ js/ui/app/web/dashboard/tabs/ExtraInfoTabView.jsx
@@ -1,18 +1,20 @@
 import React, {Component} from 'react'
 import {Tabs, Tab} from 'material-ui/Tabs'
 import {Row, Col} from 'react-flexbox-grid'
+import pureComponent from '../../mixins/pureComponent'
 import Widget from '../Widget'
 
+@pureComponent
 class ExtraInfoTabView extends Component {
   getNewsWidget() {
     return (
-      <Widget src='https://widgets.cryptocompare.com/serve/v1/coin/feed?fsym=BTC&tsym=GBP&feedType=CoinTelegraph'/>
+      <Widget src={`https://widgets.cryptocompare.com/serve/v1/coin/feed?fsym=BTC&tsym=${this.props.currency}&feedType=CoinTelegraph`}/>
     )
   }
 
   getPriceWidget() {
     return (
-      <Widget src='https://widgets.cryptocompare.com/serve/v3/coin/chart?fsym=BTC&tsyms=USD,EUR,GBP'/>
+      <Widget src={`https://widgets.cryptocompare.com/serve/v3/coin/chart?fsym=BTC&tsyms=USD,EUR,GBP`}/>
     )
   }
 
