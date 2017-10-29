fcbb0f0123cf7b5a8ae71ff78e7df43cf5564809
ppoliani <ppoliani@gmail.com>
2017-10-29 12:48:59 +0000

    use custom theme colors

--- js/ui/index.js
+++ js/ui/index.js
@@ -5,7 +5,9 @@ import {
   AppRegistry,
   StyleSheet
 } from 'react-native'
-import {Container} from 'native-base'
+import {StyleProvider} from 'native-base'
+import getTheme from './native-base-theme/components'
+import commonTheme from './native-base-theme/variables/commonColor';
 import Router from './app/native/Router'
 import configureStore from './app/data/'
 import Layout from './app/native/layout/Layout'
@@ -16,9 +18,11 @@ export default class Cryprosis extends Component {
   render() {
     const LayoutHOC = Layout(Router);
     return (
-      <Provider store={store}>
-        <Router />
-      </Provider>
+      <StyleProvider  style={getTheme(commonTheme)}> 
+        <Provider store={store}>
+          <Router />
+        </Provider>
+      </StyleProvider>
     );
   }
 }
